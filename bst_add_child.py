e554e1bc82416806396e583f16b12ecf61970f88
Joanne Yeung <jttyeung@gmail.com>
2017-04-08 21:54:27 -0700

    added doctest and partial solution

--- bst_add_child.py
+++ bst_add_child.py
@@ -7,14 +7,11 @@ class Node(object):
         self.right = right
 
     def insert(self, new_data):
-        """ Inserts a new node with `new_data` to BST tree rooted here.
-        A “balanced” binary search tree is one where the nodes are equitably spread out to guarantee O(log n) search. For this code challenge, you should not try to re-arrange the tree to rebalance it after adding an item; instead, you should simply find the correct place in the current tree to add it.
+        """ Inserts a new node with 'new_data' to BST tree rooted here.
+        A "balanced" binary search tree is one where the nodes are equitably spread out to guarantee O(log n) search. For this code challenge, you should not try to re-arrange the tree to rebalance it after adding an item; instead, you should simply find the correct place in the current tree to add it.
 
 
-        >>> t = Node(4,
-                Node(2, Node(1), Node(3)),
-                Node(7, Node(5), Node(8))
-                )
+        >>> t = Node(4, Node(2, Node(1), Node(3)), Node(7, Node(5), Node(8)))
 
         >>> t.insert(0)
         >>> t.left.left.left.data == 0
@@ -35,3 +32,29 @@ class Node(object):
         True
         """
 
+        def _insert(self, new_data):
+            if self.right is None and self.left is None:
+                return
+
+            if new_data > self.data and self.right is None:
+                self.right = new_data
+            elif new_data < self.data and self.left is None:
+                self.left = new_data
+            elif new_data > self.data and self.right is not None:
+                return _insert(self.right, new_data)
+            else:
+                return _insert(self.left, new_data)
+
+
+
+        return _insert(self.data, new_data)
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    results = doctest.testmod()
+
+    if results.failed == 0:
+        print "ALL TESTS PASSED!"
