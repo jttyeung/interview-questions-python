4b75c63467460add0a102e6e266ccc1010d85bd2
Joanne Yeung <jttyeung@gmail.com>
2017-04-11 12:29:22 -0700

    revised return logic

--- highest_product_of_three.py
+++ highest_product_of_three.py
@@ -13,10 +13,13 @@ def highest_product_of_three(list_of_ints):
     >>> highest_product_of_three([10,2,5])
     100
 
+    >>> highest_product_of_three([5,4,3,2,1])
+    60
+
     """
 
-    # Runtime:
-    # Spacetime:
+    # Runtime: O(n)
+    # Spacetime: O(1)
 
     highest_product = list_of_ints[0] * list_of_ints[1] * list_of_ints[2]
     highest = list_of_ints[0]
@@ -27,24 +30,30 @@ def highest_product_of_three(list_of_ints):
     if len(list_of_ints) == 3:
         return highest_product
 
+
+    for i in range(2,len(list_of_ints)-1):
+        product = list_of_ints[i] * list_of_ints[i + 1]
+        current_num = list_of_ints[i]
+
+        if current_num > highest:
+            highest = current_num
+            if product > highest_two:
+                highest_two = product
+        elif current_num < lowest:
+            lowest = current_num
+            if product < lowest_two:
+                lowest_two = product
+
+    if highest_two * highest > lowest_two * lowest:
+        highest_product = highest_two * highest
+    elif highest_two * highest < lowest_two * lowest:
+        highest_product = lowest_two * lowest
+    elif highest_two * lowest < lowest_two * highest:
+        highest_product = lowest_two * highest
     else:
-        for i in range(len(list_of_ints)-1):
-            product = list_of_ints[i] * list_of_ints[i + 1]
-            current_num = list_of_ints[i]
+        highest_product = highest_two * lowest
 
-            if current_num > highest:
-                highest = current_num
-                if product > highest_two:
-                    highest_two = product
-            elif current_num < lowest:
-                lowest = current_num
-                if product < lowest_two:
-                    lowest_two = product
-
-        if highest_two * highest > lowest_two * lowest:
-            return highest_two * highest
-        else:
-            return lowest_two * lowest
+    return highest_product
 
 
 
