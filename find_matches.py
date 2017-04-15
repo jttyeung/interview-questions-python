a6a8df0875f415497b2f625dd062558ac5ad4072
Joanne Yeung <jttyeung@gmail.com>
2017-04-14 18:25:04 -0700

    updated to support search in multi-column emails or phones

--- find_matches.py
+++ find_matches.py
@@ -12,7 +12,7 @@ def find_matches(input_file, matching_type):
     Matching types:
     > email
     > phone
-    > email&&phone
+    > email_phone
     """
 
     with open(input_file, 'rU') as csv_file_in:
@@ -21,12 +21,13 @@ def find_matches(input_file, matching_type):
         rownum = 0
         header = None
         email_col = None
+        email_col_2 = None
         phone_col = None
         phone_col_2 = None  # For secondary phone column if it exists
 
         # Keep track of entry duplicates/key assignments
         ids = {}
-        key = 0
+        key = ''
         id = 1
 
         # Declare the header for the file
@@ -38,7 +39,9 @@ def find_matches(input_file, matching_type):
 
         # Get the column number for email
         for col in range(len(header)):
-            if 'email' in header[col].lower():
+            if 'email2' in header[col].lower():
+                email_col_2 = col
+            if 'email1' in header[col].lower() or 'email' == header[col].lower():
                 email_col = col
 
         # Get the column number for phone(s)
@@ -60,18 +63,41 @@ def find_matches(input_file, matching_type):
 
             # Write subsequent rows to new file with id value from ids dictionary
             for row in reader:
+
                 # Clean phone numbers to 10 digit integers before finding matches
-                if matching_type == 'phone':
-                    key = re.sub('\D+','',row[phone_col])
-                    if len(key) > 10:
-                        key = key[1:]
+                # Assigns the phone number as the dictionary key
+                if matching_type == 'phone' or matching_type == 'email_phone':
+                    key1 = re.sub('\D+','',row[phone_col])
+                    if len(key1) > 10:
+                        key1 = key1[1:]
 
-                if matching_type == 'email':
-                    key = row[email_col].lower()
+                    key2 = re.sub('\D+','',row[phone_col_2])
+                    if len(key2) > 10:
+                        key2 = key2[1:]
 
+                # Assigns the email as the dictionary key
+                if matching_type == 'email' or matching_type == 'email_phone':
+                    key1 = row[email_col].lower()
+                    key2 = row[email_col_2].lower()
 
-                ids[key] = ids.get(key, id)
-                row = [ids[key]] + row
+                # Pairs the email/phone with an id in the dictionary if one exists
+                if key1:
+                    ids[key1] = ids.get(key1, id)
+                if key2:
+                    ids[key2] = ids.get(key2, id)
+
+                # Determines the id values in each row
+                if key1 and key2:
+                    if ids[key1] != ids[key2]:
+                        row_ids = '{},{}'.format(ids[key1], ids[key2])
+                    else:
+                        row_ids = ids[key1]
+                elif not key1:
+                    row_ids = ids[key2]
+                elif not key2:
+                    row_ids = ids[key1]
+
+                row = [row_ids] + row
                 writer.writerow(row)
                 id += 1
 
