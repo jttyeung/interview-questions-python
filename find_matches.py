226e64f9bfe04e260e0de3e784d37cc5c5eaa0e7
Joanne Yeung <jttyeung@gmail.com>
2017-04-17 17:35:49 -0700

    added email & phone match funcs, added write_csv func

--- find_matches.py
+++ find_matches.py
@@ -24,192 +24,100 @@ class FindMatches(object):
         self.phone_col = None
         self.phone_col_2 = None
 
+        # Keep track of entry duplicates/key assignments
+        self.ids = {}
+        self.id = 1
+
         # Open the file with a csv reader
-        with open(self.input_file, 'rU') as csv_file_in:
-            reader = csv.reader(csv_file_in)
+        csv_file_in = open(self.input_file, 'rU')
+        reader = csv.reader(csv_file_in)
 
-            # Declare the header for the file
-            for row in reader:
-                if self.rownum == 0:
-                    self.header = row
-                    self.rownum += 1
-                    break
+        # Declare the header for the file
+        for row in reader:
+            if self.rownum == 0:
+                self.header = row
+                self.rownum += 1
+                break
 
-            # Get the column number(s) for email
-            for col in range(len(self.header)):
-                if 'email2' in self.header[col].lower():
-                    self.email_col_2 = col
-                if 'email1' in self.header[col].lower() or 'email' == self.header[col].lower():
-                    self.email_col = col
+        # Get the column number(s) for email
+        for col in range(len(self.header)):
+            if 'email2' in self.header[col].lower():
+                self.email_col_2 = col
+            if 'email1' in self.header[col].lower() or 'email' == self.header[col].lower():
+                self.email_col = col
 
-            # Get the column number(s) for phone
-            for col in range(len(self.header)):
-                if 'phone2' in self.header[col].lower():
-                    self.phone_col_2 = col
-                if 'phone1' in self.header[col].lower() or 'phone' == self.header[col].lower():
-                    self.phone_col = col
+        # Get the column number(s) for phone
+        for col in range(len(self.header)):
+            if 'phone2' in self.header[col].lower():
+                self.phone_col_2 = col
+            if 'phone1' in self.header[col].lower() or 'phone' == self.header[col].lower():
+                self.phone_col = col
 
-        # Run tests based on match type
-        if self.matching_type == 'email':
-            self.email_match()
-        elif self.matching_type == 'phone':
-            self.phone_match()
-        elif self.matching_type == 'email_phone':
-            self.email_phone_match()
-        else:
-            print "Please use a valid matching type: 'email', 'phone', or 'email_phone'."
+        # Runs matching type tests based on match type given and writes it to a csv
+        for row in reader:
+            if self.matching_type == 'email':
+                self.email_match(row)
+                self.write_csv(row, self.header)
+            elif self.matching_type == 'phone':
+                self.phone_match(row)
+                self.write_csv(row, self.header)
+            elif self.matching_type == 'email_phone':
+                self.email_match(row)
+                self.phone_match(row)
+                self.write_csv(row, self.header)
+            else:
+                print "Please use a valid matching type: 'email', 'phone', or 'email_phone'."
+            self.id += 1
 
-    def email_match(self):
-        print 'Hello'
+###  could probably combine both email and phone match into one thing.
+###  use a parameter to take matching type into function and use it?
 
-    def phone_match(self):
-        print 'Phone'
+    def email_match(self, row):
+        """ Creates email tuples and insert unique tuples into the ids dictionary. """
 
-    def write_csv(self):
-        pass
-#     with open(input_file, 'rU') as csv_file_in:
-#         reader = csv.reader(csv_file_in)
-
-#         rownum = 0
-#         # header = None
-#         email_col = None
-#         email_col_2 = None  # For secondary email column if it exists
-#         phone_col = None
-#         phone_col_2 = None  # For secondary phone column if it exists
-#         key1 = None
-#         key2 = None
-#         key3 = None
-#         key4 = None
-
-#         # Keep track of entry duplicates/key assignments
-#         ids = {}
-#         key = ''
-#         id = 1
-
-#         # Declare the header for the file
-#         for row in reader:
-#             if rownum == 0:
-#                 header = row
-#                 rownum += 1
-#                 break
-
-#         # Get the column number for email
-#         for col in range(len(header)):
-#             if 'email2' in header[col].lower():
-#                 email_col_2 = col
-#             if 'email1' in header[col].lower() or 'email' == header[col].lower():
-#                 email_col = col
-
-#         # Get the column number for phone(s)
-#         for col in range(len(header)):
-#             if 'phone2' in header[col].lower():
-#                 phone_col_2 = col
-#             if 'phone1' in header[col].lower() or 'phone' == header[col].lower():
-#                 phone_col = col
+        # Creates a sorted list of emails
+        ids_key = sorted([row[self.email_col].lower(), row[self.email_col_2].lower()])
+        # Puts list of emails into a tuple for the dictionary
+        ids_key = tuple(ids_key)
+        # Places keys into dictionary
+        # If key exists the key-value pair does not change,
+        # otherwise place it in the dictionary and assign it a new id.
+        self.ids[ids_key] = self.ids.get(ids_key, self.id)
 
 
+    def phone_match(self, row):
+        """ Creates phone tuples and insert unique tuples into the ids dictionary. """
 
-# def write_csv(header):
-#     """ Returns a copy of the original csv with the unique identifier of the person each row represents prepended to the row. """
-
-#     # Creates a csv writer
-#     with open('output_file.csv', 'w') as csv_file_out:
-#         writer = csv.writer(csv_file_out)
-
-#         # Add 'id' column to header
-#         header = ['id'] + header
-
-#         # Write header row to new file
-#         writer.writerow(header)
+        # Creates a sorted list of emails
+        ids_key = sorted([row[self.phone_col].lower(), row[self.phone_col_2].lower()])
+        # Puts list of emails into a tuple for the dictionary
+        ids_key = tuple(ids_key)
+        # Places keys into dictionary
+        # If key exists the key-value pair does not change,
+        # otherwise place it in the dictionary and assign it a new id.
+        self.ids[ids_key] = self.ids.get(ids_key, self.id)
 
 
+    def write_csv(self, row, header, row_is_header=True):
+        # Create a new file with a csv writer
+        csv_file_out = open('output_file.csv', 'w')
+        writer = csv.writer(csv_file_out)
 
-#     # Write subsequent rows to new file with id value from ids dictionary
-#     for row in reader:
+        while row_is_header:
+            # Add 'id' column to header
+            header = ['id'] + self.header
 
-#         # Clean phone numbers to 10 digit integers before finding matches
-#         # Assigns the phone number as the dictionary key
-#         if matching_type == 'phone':
-#             if phone_col:
-#                 key1 = re.sub('\D+','',row[phone_col])
-#                 if len(key1) > 10:
-#                     key1 = key1[1:]
+            # Write header row to new file
+            writer.writerow(header)
 
-#             if phone_col_2:
-#                 key2 = re.sub('\D+','',row[phone_col_2])
-#                 if len(key2) > 10:
-#                     key2 = key2[1:]
+            row_is_header = False
 
+        # Gets the row id value from the dictionary:
+        row_id = self.ids[]
 
-#         # Assigns the email as the dictionary key
-#         if matching_type == 'email':
-#             if email_col:
-#                 key1 = row[email_col].lower()
-#             if email_col_2:
-#                 key2 = row[email_col_2].lower()
-
-#         if matching_type == 'email' or matching_type == 'phone':
-#             # Pairs the email/phone with an id in the dictionary if one exists
-#             if key1:
-#                 ids[key1] = ids.get(key1, id)
-#             if key2:
-#                 ids[key2] = ids.get(key2, id)
-
-#             # Determines the id values in each row
-#             if key1 and key2:
-#                 if ids[key1] != ids[key2]:
-#                     # row_ids = '{},{}'.format(ids[key1], ids[key2])
-#                     row_ids = ids[key1]
-#                 else:
-#                     row_ids = ids[key1]
-#             elif not key1 and not key2:
-#                 row_ids = None
-#             elif not key1:
-#                 row_ids = ids[key2]
-#             elif not key2:
-#                 row_ids = ids[key1]
-
-
-#         if matching_type == 'email_phone':
-#             if phone_col:
-#                 key1 = re.sub('\D+','',row[phone_col])
-#                 if len(key1) > 10:
-#                     key1 = key1[1:]
-
-#             if phone_col_2:
-#                 key2 = re.sub('\D+','',row[phone_col_2])
-#                 if len(key2) > 10:
-#                     key2 = key2[1:]
-
-#             if email_col:
-#                 key3 = row[email_col].lower()
-#             if email_col_2:
-#                 key4 = row[email_col_2].lower()
-
-#             row_ids = set()
-
-#             if key1 and key3:
-#                 key_combo_1 = key1 + key3
-#                 ids[key_combo_1] = ids.get(key_combo_1, id)
-#                 row_ids.add(ids[key_combo_1])
-#             if key1 and key4:
-#                 key_combo_2 = key1 + key4
-#                 ids[key_combo_2] = ids.get(key_combo_2, id)
-#                 row_ids.add(ids[key_combo_2])
-#             if key2 and key3:
-#                 key_combo_3 = key2 + key3
-#                 ids[key_combo_3] = ids.get(key_combo_3, id)
-#                 row_ids.add(ids[key_combo_3])
-#             if key2 and key4:
-#                 key_combo_4 = key2 + key4
-#                 ids[key_combo_4] = ids.get(key_combo_4, id)
-#                 row_ids.add(ids[key_combo_4])
-
-#             row_ids = ','.join(str(s) for s in row_ids)
-
-#         row = [row_ids] + row
-#         writer.writerow(row)
-#         id += 1
+        # Writes the new row: id of the row + copied csv row
+        new_row = [row_id] + row
+        writer.writerow(new_row)
 
 
 FindMatches(input_file=sys.argv[1], matching_type=sys.argv[2])
