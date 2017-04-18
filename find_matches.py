fbee6e0a64dbc931c76947d322d0fd97f4c87423
Joanne Yeung <jttyeung@gmail.com>
2017-04-18 01:54:32 -0700

    fixed phone and email matching and id-ing

--- find_matches.py
+++ find_matches.py
@@ -58,60 +58,93 @@ class FindMatches(object):
             if 'phone1' in self.header[col].lower() or 'phone' == self.header[col].lower():
                 self.phone_col = col
 
-        # Runs matching type tests based on match type given
-        for row in reader:
-            if self.matching_type == 'email':
-                self.email_match(row)
-            elif self.matching_type == 'phone':
-                self.phone_match(row)
-            elif self.matching_type == 'email_phone':
-                self.email_match(row)
-                self.phone_match(row)
+        # # Runs matching type tests based on match type given
+        # for row in reader:
+        #     if self.matching_type == 'email':
+        #         email_key = self.email_match(row)
+        #         self.write_csv(row, email_key, self.header)
+        #     elif self.matching_type == 'phone':
+        #         phone_key = self.phone_match(row)
+        #         self.write_csv(row, phone_key, self.header)
+        #     elif self.matching_type == 'email_phone':
+        #         self.email_match(row)
+        #         self.phone_match(row)
 
-            # Increments id in ids dictionary for unique row ids
-            self.id += 1
+        #     # Increments id in ids dictionary for unique row ids
+        #     self.id += 1
 
         # Write to csv after all ids are assigned
-        self.write_csv(self.reader, self.header)
+        self.write_csv(reader, self.header)
 
 
-    def email_match(self, row):
+    # def email_match(self, row):
+    #     """ Creates email tuples and insert unique tuples into the ids dictionary. """
+
+    #     # Creates a sorted list of emails
+    #     # ids_key = sorted([row[self.email_col].lower(), row[self.email_col_2].lower()])
+    #     ids_key = row[self.email_col].lower()
+    #     self.add_key_to_dict(ids_key)
+    #     if self.email_col_2:
+    #         ids_key = row[self.email_col_2].lower()
+    #         self.add_key_to_dict(ids_key)
+    #     # Puts list of emails into a tuple for the dictionary
+    #     # ids_key = tuple(ids_key)
+    #     return ids_key
+
+
+    def email_match(self, row, column):
         """ Creates email tuples and insert unique tuples into the ids dictionary. """
 
-        # Creates a sorted list of emails
-        ids_key = sorted([row[self.email_col].lower(), row[self.email_col_2].lower()])
-        # Puts list of emails into a tuple for the dictionary
-        ids_key = tuple(ids_key)
-        # Places keys into dictionary
-        # If key exists the key-value pair does not change,
-        # otherwise place it in the dictionary and assign it a new id.
-        self.ids[ids_key] = self.ids.get(ids_key, self.id)
+        ids_key = row[column].lower()
+        self.add_key_to_dict(ids_key)
+
+        return ids_key
 
 
-    def phone_match(self, row):
+    def phone_match(self, row, column):
         """ Creates phone tuples and insert unique tuples into the ids dictionary. """
 
         # Removes formatting of phone numbers for direct comparison
-        format_phone_col = re.sub('\D+','',row[self.phone_col])
-        format_phone_col_2 = re.sub('\D+','',row[self.phone_col_2])
+        format_phone_col = re.sub('\D+','',row[column])
 
         if len(format_phone_col) > 10:
             format_phone_col[1:]
-        if len(format_phone_col_2) > 10:
-            format_phone_col_2[1:]
 
-        # Creates a sorted list of emails
-        ids_key = sorted([format_phone_col, format_phone_col_2])
-        # Puts list of emails into a tuple for the dictionary
-        ids_key = tuple(ids_key)
-        # Places keys into dictionary
-        # If key exists the key-value pair does not change,
-        # otherwise place it in the dictionary and assign it a new id.
+        ids_key = format_phone_col
+        self.add_key_to_dict(ids_key)
+
+        return ids_key
+
+    # def phone_match(self, row):
+    #     """ Creates phone tuples and insert unique tuples into the ids dictionary. """
+
+    #     # Removes formatting of phone numbers for direct comparison
+    #     format_phone_col = re.sub('\D+','',row[self.phone_col])
+    #     format_phone_col_2 = re.sub('\D+','',row[self.phone_col_2])
+
+    #     if len(format_phone_col) > 10:
+    #         format_phone_col[1:]
+    #     if len(format_phone_col_2) > 10:
+    #         format_phone_col_2[1:]
+
+    #     # Creates a sorted list of emails
+    #     # ids_key = sorted([format_phone_col, format_phone_col_2])
+    #     ids_key = format_phone_col
+    #     self.add_key_to_dict(ids_key)
+    #     if self.format_phone_col_2:
+    #         ids_key = format_phone_col_2
+    #         self.add_key_to_dict(ids_key)
+
+
+    def add_key_to_dict(self, ids_key):
+        """ Places keys into a dictionary. If the key exists the key-value pair does not change. Otherwise, place it in the dictionary and assign it a new id. """
+
         self.ids[ids_key] = self.ids.get(ids_key, self.id)
 
 
     def write_csv(self, reader, header, row_is_header=True):
         """ Using a csv writer, creates a copy of the file with unique ids prepended to each row. """
+
         csv_file_out = open('output_file.csv', 'w')
         writer = csv.writer(csv_file_out)
 
@@ -124,13 +157,60 @@ class FindMatches(object):
 
             row_is_header = False
 
-        # Gets the row id value from the dictionary:
+        # Runs matching type tests based on match type given
         for row in reader:
-            row_id = phone_match
+            row_id = None
 
-        # # Writes the new row: id of the row + copied csv row
-        # new_row = [row_id] + row
-        # writer.writerow(new_row)
+            if self.matching_type == 'email':
+
+                if row[self.email_col_2]:
+                    ids_key = self.email_match(row, self.email_col_2)
+
+                    if self.ids.get(ids_key):
+                        row_id = self.ids.get(ids_key)
+
+                if row[self.email_col] and row_id is None:
+                    ids_key = self.email_match(row, self.email_col)
+                    row_id = self.ids.get(ids_key, self.id)
+
+
+            elif self.matching_type == 'phone':
+
+                if row[self.phone_col_2]:
+                    ids_key = self.phone_match(row, self.phone_col_2)
+
+                    if self.ids.get(ids_key):
+                        row_id = self.ids.get(ids_key)
+
+                if row[self.phone_col] and row_id is None:
+                    ids_key = self.phone_match(row, self.phone_col)
+                    row_id = self.ids.get(ids_key, self.id)
+
+
+
+            elif self.matching_type == 'email_phone':
+                self.email_match(row)
+                self.phone_match(row)
+
+            # Writes the new row: id of the row + copied csv row
+            # row_id = self.ids[ids_key]
+            new_row = [row_id] + row
+            writer.writerow(new_row)
+
+            # Increments id in ids dictionary for unique row ids
+            self.id += 1
+
+        # Gets the row id value from the dictionary:
+        # for row in reader:
+            # if self.matching_type == 'email':
+            #     row_id = ids[email]
+            # elif self.matching_type == 'phone':
+            #     self.phone_match(row)
+            # elif self.matching_type == 'email_phone':
+            #     self.email_match(row)
+            #     self.phone_match(row
+
+
 
 
 FindMatches(input_file=sys.argv[1], matching_type=sys.argv[2])
