4865d626749b1b9c484f397a82a9f38e33a31736
Joanne Yeung <jttyeung@gmail.com>
2017-05-02 23:26:00 -0700

    updated solution to actual mode definition

--- find_mode.py
+++ find_mode.py
@@ -1,34 +1,42 @@
 def find_mode(arr):
     """ Finds the mode(s) of the array.
 
-    >>> sorted(find_mode([3,5,6,2,6,7,8,3,6,6]))
-    [3, 6]
+    >>> find_mode([3,5,6,2,6,7,8,3,6,6])
+    set([6])
 
     >>> find_mode([1,2,3,4,5])
-    []
+    set([1, 2, 3, 4, 5])
 
-    >>> sorted(find_mode([2,1,2,1]))
-    [1, 2]
+    >>> find_mode([2,1,2,1])
+    set([1, 2])
 
     >>> find_mode([1,2,3,2,4])
-    [2]
+    set([2])
 
     >>> find_mode([])
-    []
+    set([])
 
     """
 
     if not arr:
-        return []
+        return set([])
     if len(arr) < 2:
-        return arr[0]
+        return set([arr[0]])
 
     nums = {}
+    mode = None
+    n = set()
 
     for i in arr:
         nums[i] = nums.get(i, 0) + 1
 
-    return [letter for letter, val in nums.iteritems() if val > 1]
+    for num, val in nums.iteritems():
+        if val > mode:
+            mode = val
+            n = set([num])
+        if val == mode and num not in n:
+            n.add(num)
+    return n
 
 
 
