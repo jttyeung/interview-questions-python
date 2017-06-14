17244a38913625cfeaa6ab5eec1f75c1c74cd45c
Joanne Yeung <jttyeung@gmail.com>
2017-06-14 14:24:09 -0700

    added prompt and unit tests

---
+++ most_frequent_word.py
@@ -0,0 +1,27 @@
+def most_frequent_word(str):
+    """ Find k most frequent words in a string of words.
+
+    >>> most_frequent_word('hello my name is hello joanne')
+    'hello'
+
+    >>> most_frequent_word('hello my name is hello joanne is')
+    'hello', 'is'
+
+    >>> most_frequent_word('hello my name is joanne')
+    None
+    """
+
+    
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    results = doctest.testmod()
+    if not results.failed:
+        print 'All tests passed!'
