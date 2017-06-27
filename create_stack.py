0a971543ccbd83ce26cb87031f44619b00fa1247
Joanne Yeung <jttyeung@gmail.com>
2017-06-26 21:45:28 -0400

    added stack minimum method

--- create_stack.py
+++ create_stack.py
@@ -1,7 +1,3 @@
-class StackEmptyError(IndexError):
-    """Attempt to pop an empty stack."""
-
-
 class Stack(object):
     """LIFO stack.
 
@@ -25,23 +21,39 @@ class Stack(object):
 
     def __init__(self):
         self.items = []
+        self.min_stack = []
+
 
     def __repr__(self):
-        if not self._list:
+        if not self.items:
             return "<Stack (empty)>"
         else:
             return "<Stack tail=%s length=%d>" % (
-                self._list[-1], len(self._list))
+                self.items[-1], len(self.items))
+
 
     def push(self, item):
         """Add item to end of stack."""
 
         self.items.append(item)
 
+        if self.min_stack == [] or self.min_stack[-1] > item:
+            self.min_stack.append(item)
+
+        else:
+            self.min_stack.append(self.min_stack[-1])
+
+
     def pop(self):
         """Remove item from end of stack and return it."""
 
+        if self.is_empty():
+            return IndexError('pop from empty list')
+
+        self.min_stack.pop()
+
         return self.items.pop()
+
 
     def __iter__(self):
         """Allow iteration over list.
@@ -51,11 +63,13 @@ class Stack(object):
         like "for item in my_stack", and it will pop
         successive items off.
         """
+
         while True:
             try:
                 yield self.pop()
             except StackEmptyError:
                 raise StopIteration
+
 
     def length(self):
         """Return length of stack::
@@ -64,25 +78,27 @@ class Stack(object):
             >>> s.length()
             0
 
-            >>> s.push("dog")
-            >>> s.push("cat")
-            >>> s.push("fish")
+            >>> s.push(3)
+            >>> s.push(4)
+            >>> s.push(5)
 
             >>> s.length()
             3
         """
+
         count = 0
         for item in self.items:
             count += 1
         return count
 
+
     def empty(self):
         """Empty stack::
 
             >>> s = Stack()
-            >>> s.push("dog")
-            >>> s.push("cat")
-            >>> s.push("fish")
+            >>> s.push(4)
+            >>> s.push(3)
+            >>> s.push(2)
 
             >>> s.length()
             3
@@ -92,7 +108,10 @@ class Stack(object):
             >>> s.length()
             0
         """
+
         self.items = []
+        self.min_stack = []
+
 
     def is_empty(self):
         """Is stack empty?
@@ -102,14 +121,62 @@ class Stack(object):
             >>> s.is_empty()
             True
 
-            >>> s.push("dog")
-            >>> s.push("cat")
-            >>> s.push("fish")
+            >>> s.push(3)
+            >>> s.push(2)
+            >>> s.push(4)
 
             >>> s.is_empty()
             False
         """
-        return not self.items
+
+        return self.items == []
+
+
+    def find_min(self):
+        """ Returns the minimum value of a numerical stack.
+
+        >>> s = Stack()
+        >>> s.push(2)
+        >>> s.push(1)
+        >>> s.push(3)
+        >>> s.push(-1)
+        >>> s.find_min()
+        -1
+
+        >>> s2 = Stack()
+        >>> s2.push(2)
+        >>> s2.push(1)
+        >>> s2.push(3)
+        >>> s2.find_min()
+        1
+
+        >>> s3 = Stack()
+        >>> s3.push(2)
+        >>> s3.push(1)
+        >>> s3.push(3)
+        >>> s3.push(3)
+        >>> s3.push(1)
+        >>> s3.find_min()
+        1
+
+        >>> s3.pop()
+        1
+        >>> s3.find_min()
+        1
+
+        >>> s3.pop()
+        3
+        >>> s3.pop()
+        3
+        >>> s3.pop()
+        1
+        >>> s3.find_min()
+        2
+        """
+
+        if not self.is_empty():
+            return self.min_stack[-1]
+
 
 
 if __name__ == "__main__":
