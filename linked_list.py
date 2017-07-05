1e443a253784c1dac8c4a13a5fbde7d925c8d350
Joanne Yeung <jttyeung@gmail.com>
2017-07-04 21:15:20 -0700

    added problem

---
+++ linked_list.py
@@ -0,0 +1,25 @@
+""" Implement a singly linked list. """
+
+class Node(object):
+
+    def __init__(self, data=None, nxt=None):
+        self.data = data
+        self.next = nxt
+
+
+class LinkedList(object):
+
+    def __init__(self, head=None:
+        self.head = head
+        self.tail = tail
+
+    def insert():
+
+
+    def size():
+
+
+    def search():
+
+
+    def delete():
