d31ec13c95ca2717f01fb14635c3657a04a6177b
Joanne Yeung <jttyeung@gmail.com>
2017-07-12 23:23:11 -0700

    added problem

---
+++ find_longest_word.py
@@ -0,0 +1,11 @@
+def find_longest_word(words):
+    """Return longest word in list of words.
+
+    >>> find_longest_word(["hi", "hello"])
+    5
+
+    >>> find_longest_word(["Balloonicorn", "Hackbright"])
+    12
+
+    """
+
