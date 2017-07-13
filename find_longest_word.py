34cdca37bf7e15d440871c3418afe5c797287c0c
Joanne Yeung <jttyeung@gmail.com>
2017-07-12 23:28:00 -0700

    added solution and tests

--- find_longest_word.py
+++ find_longest_word.py
@@ -1,11 +1,25 @@
+import unittest
+
+
+
 def find_longest_word(words):
-    """Return longest word in list of words.
+    """Return longest word in list of words."""
 
-    >>> find_longest_word(["hi", "hello"])
-    5
+    longest = 0
 
-    >>> find_longest_word(["Balloonicorn", "Hackbright"])
-    12
+    for word in words:
+        longest = max(len(word), longest)
 
-    """
+    return longest
 
+
+class test_solution(unittest.TestCase):
+
+    def test_find_longest_word(self):
+        self.assertEqual(find_longest_word(["hi", "hello"]), 5)
+        self.assertEqual(find_longest_word(["Balloonicorn", "Hackbright"]), 12)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
