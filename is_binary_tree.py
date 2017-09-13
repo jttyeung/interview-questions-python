d568720c8be26c1f33f048acc5708dcf79ae5d07
Joanne Yeung <jttyeung@gmail.com>
2017-09-12 22:10:31 -0700

    added solution

--- is_binary_tree.py
+++ is_binary_tree.py
@@ -1,8 +1,31 @@
 import unittest
 
+
 def is_bst(root):
-    """ Given the root of a binary tree, return true if it is a binary search tree. """
-    pass
+    """
+    Given the root of a binary tree, return true if it is a binary search tree.
+    """
+
+    def is_bst_recursive(root, smallest, largest):
+
+        if root is None:
+            return True
+
+        if largest is not None and root.data > largest:
+            return False
+
+        if smallest is not None and root.data < smallest:
+            return False
+
+        if not is_bst_recursive(root.left, smallest, root.data):
+            return False
+
+        if not is_bst_recursive(root.right, root.data, largest):
+            return False
+
+        return True
+
+    return is_bst_recursive(root, smallest=None, largest=None)
 
 
 class Node(object):
@@ -23,7 +46,6 @@ class Tests(unittest.TestCase):
     def test_is_bst(self):
         self.assertFalse(is_bst(self.create_bst_1()))
         self.assertTrue(is_bst(self.create_bst_2()))
-
 
 
 if __name__ == '__main__':
