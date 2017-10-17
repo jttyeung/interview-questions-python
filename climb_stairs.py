6e5e244bca7c68d9cf2666cc922fd5da32e48ace
Joanne Yeung <jttyeung@gmail.com>
2017-10-17 13:06:04 -0700

    add tabulation method --incomplete

--- climb_stairs.py
+++ climb_stairs.py
@@ -16,7 +16,7 @@ def climb_stairs(n):
 
 
 def climb_stairs_dp(n):
-    """ A fox needs to climb n number of steps. It can jump up 1 step, 2 steps, or 3 steps at a time. How many possible ways are there to get to the top of n steps? Solve with dynamic programming. """
+    """ A fox needs to climb n number of steps. It can jump up 1 step, 2 steps, or 3 steps at a time. How many possible ways are there to get to the top of n steps? Solve with dynamic programming using the memoization method. """
 
     cache = { 2: 2, 1: 1, 0: 1 }
 
@@ -29,6 +29,20 @@ def climb_stairs_dp(n):
 # Runtime: O(n)
 
 
+def climb_stairs_tab(n, steps):
+    """ A fox needs to climb n number of steps. It can jump up 1 step, 2 steps, or 3 steps at a time. How many possible ways are there to get to the top of n steps? Solve with dynamic programming using the tabulation method. """
+    result = [0 for x in range(steps)]
+    result[0] = 1
+
+    for i in range(len(steps)):
+        for j in range(steps[i], len(steps)):
+            sum = 0
+            for k in range(0, i+1):
+                sum += result[j - steps[k]];
+            result[j] = sum
+
+    return result[n]
+
 
 
 class Testing(unittest.TestCase):
@@ -37,8 +51,12 @@ class Testing(unittest.TestCase):
         self.assertEqual(climb_stairs(8), 81)
 
     def test_climb_stairs_dp(self):
-        self.assertEqual(climb_stairs(6), 24)
-        self.assertEqual(climb_stairs(8), 81)
+        self.assertEqual(climb_stairs_dp(6), 24)
+        self.assertEqual(climb_stairs_dp(8), 81)
+
+    def test_climb_stairs_tab(self):
+        self.assertEqual(climb_stairs_tab(6, [1, 2, 3]), 24)
+        self.assertEqual(climb_stairs_tab(10, [2, 3, 5]), 14)
 
 
 
