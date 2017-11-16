c61a6521789578035c1a8281e5f63aab25f1ea40
Joanne Yeung <jttyeung@gmail.com>
2017-11-15 17:50:01 -0800

    add unit testing

--- award_budget_cuts.py
+++ award_budget_cuts.py
@@ -25,40 +25,15 @@
 
 # [output] double
 
-
-# grantsArray = [2, 100, 50, 120, 1000]
-# newBudget = 190
-# # 47
-
-# grantsArray = [2,4]
-# newBudget = 3
-# # 1.5
-
-# grantsArray = [2,4,6]
-# newBudget = 3
-# # 1
-
-# grantsArray = [2,100,50,120,1000]
-# newBudget = 190
-# # 47
-
-# grantsArray = [2,100,50,120,167]
-# newBudget = 400
-# # 128
-
-# grantsArray = [21,100,50,120,130,110]
-# newBudget = 140
-# # 23.8
-
-# grantsArray = [210,200,150,193,130,110,209,342,117]
-# newBudget = 1530
-# # 211
-
+# Steps
 # 1. Sort reversed (or not reversed)
 # 2. Stop iteration conditions:
 #     - cap < lowest grant amount
 #     - when i doesn't move (flag)
 
+
+
+import unittest
 
 
 def find_grant_cap(grantsArray, newBudget):
@@ -84,18 +59,22 @@ def find_grant_cap(grantsArray, newBudget):
     return cap
 
 
-print find_grant_cap([2, 100, 50, 120, 1000], 190)
-print find_grant_cap([2,4], 3)
-print find_grant_cap([2,4,6], 3)
-print find_grant_cap([2,100,50,120,1000], 190)
-print find_grant_cap([2,100,50,120,167], 400)
-print find_grant_cap([21,100,50,120,130,110], 140)
-print find_grant_cap([210,200,150,193,130,110,209,342,117], 1530)
+
+class Testing(unittest.TestCase):
+    def test_find_grant_cap(self):
+        self.assertEqual(find_grant_cap([2, 100, 50, 120, 1000], 190), 47)
+        self.assertEqual(find_grant_cap([2,4], 3), 1.5)
+        self.assertEqual(find_grant_cap([2,4,6], 3), 1)
+        self.assertEqual(find_grant_cap([2,100,50,120,1000], 190), 47)
+        self.assertEqual(find_grant_cap([2,100,50,120,167], 400), 128)
+        self.assertEqual(find_grant_cap([21,100,50,120,130,110], 140), 23.8)
+        self.assertEqual(find_grant_cap([210,200,150,193,130,110,209,342,117], 1530), 211)
 
 
 
 
-
+if __name__ == '__main__':
+    unittest.main()
 
 
 
